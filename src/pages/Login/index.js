eaf441920432503c89d8d6b212e6fe5677926f86
GraceTorresLeite <grace.torresleite@gmail.com>
2021-04-17 16:21:18 -0300

    add logo

--- src/pages/Login/index.js
+++ src/pages/Login/index.js
@@ -1,8 +1,22 @@
 import React from 'react';
 import './styles.css';
 
+import logo from '../../assets/logo.svg'
+import logoSchedule from '../../assets/logoSchedule.png'
+
 export default function Login() {
-    return (
-        <h1> &copy; 2021 - Schedule Beauty</h1>
+    return ( <
+        div className = 'login-container' >
+        <
+        section className = 'form' >
+
+        <
+        /section> <
+        img src = { logoSchedule }
+        alt = 'Schedule' / >
+        <
+        footer > & copy; 2021 - Schedule Beauty < /footer> <
+        /div>
+
     )
 }
